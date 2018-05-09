1caf8a48f5fb94ff23e259b213a367ab587565f2
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2018-05-08 22:47:09 -0500

    Code to insert basic product info

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Requirements:
 
 $ apt-get install mongodb
+
+
+Next steps:
+- use `get` route to pull info from the mongo db and return to user
+- set up connection to external source
+- pull name from external source
+- bundle name with pricing info and return to user
+- add a 404 response for unknown product IDs
--- app.js
+++ app.js
@@ -5,26 +5,31 @@ const url = 'mongodb://localhost:27017';
 
 const dbName = 'productPrices';
 
-// Mongo example code from https://github.com/mongodb/node-mongodb-native
-MongoClient.connect(url, function(err, client) {
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
-
 app.get('/', (req, res) => res.send('Hello Product Searcher.'))
 
 app.route('/products/:id')
     .get( function (req, res)
           {
-              // call a function here that returns the object from the
-              // data store in json
-              res.json({"id":req.params.id,"name":"The Big Lebowski (Blu-ray) (Widescreen)","current_price":{"value": 13.49,"currency_code":"USD"}});
+              // call a function here that returns the pricing data from
+              // the data store
+              price = {"value": 13.49,"currency_code":"USD"};
+
+              // then call a function that gets the name from the
+              // external source
+              name = "The Big Lebowski (Blu-ray) (Widescreen)";
+
+              // then bundle them together into a json object
+              example_object = {"id":req.params.id,"name": name,"current_price": price};
+
+              // and return to the user
+              res.json(example_object);
           })
     .put( function (req, res)
           {
+              // call a function to break up req and update price in
+              // Mongo
+
+              // send updated object back to caller
               res.send("Updated");
           }
         )
