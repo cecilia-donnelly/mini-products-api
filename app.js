5c97e12f389369c2f80aea80e2b77ccb68ce2efd
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2018-05-09 21:41:43 -0500

    Use Promises to return desired product info

--- app.js
+++ app.js
@@ -10,49 +10,42 @@ app.get('/', (req, res) => res.send('Hello Product Searcher.'))
 app.route('/products/:id')
     .get( function (req, res)
           {
+              var price_obj = {}
+              var product_id = parseInt(req.params.id);
               // Connect to the local data store and search for pricing
               // data by ID
-              MongoClient.connect(config.db_url, function(err, client) {
-                  if (err) {
-                      console.log("DEBUG: error connecting to Mongo");
-                      console.log(err);
-                  }
-                  else {
-                      const db = client.db('productPrices');
+              MongoClient.connect(config.db_url)
+                  .then( function( client ) {
+                      var db = client.db("productPrices");
 
-                      var product_id = parseInt(req.params.id);
-                      db.collection('productPrices').find(
+                      // find query is broken for some reason
+                      return db.collection("productPrices").find(
                           { "product_id": product_id }
-                      ).toArray(function (err, result) {
-                          if (err) throw err;
+                      ).toArray();
+                  }).then (function (result) {
+                      if (result[0]) {
+                          price_obj = result[0]['current_price'];
+                      }
+                      return redsky.search(
+                          product_id,
+                          config.redsky_host,
+                          config.redsky_path
+                      )
+                  }).then( function (name) {
+                      // then bundle them together into a json object
+                      product_object = {
+                          "id":req.params.id,
+                          "name": name,
+                          "current_price": price_obj
+                      };
 
-                          var price_obj = {}
-                          if (result[0]) {
-                              price_obj = result[0]['current_price'];
-                          }
-                          
-                          // external source
-                          redsky.search(product_id, config.redsky_host, config.redsky_path, price_obj)
-                              .then( function (name) {
-                                  console.log("DEBUG: do I still have price?");
-                                  console.log(price_obj);
-                                  console.log("DEBUG: did my promise return what I wanted?");
-                                  console.log(name);
-                                  // then bundle them together into a json object
-                                  product_object = {
-                                      "id":req.params.id,
-                                      "name": name,
-                                      "current_price": price_obj
-                                  };
-
-                                  // and return to the user
-                                  res.json(product_object);
-                              });
-                      });
-                  }
-              });
-
-          })
+                      // and return to the user
+                      res.json(product_object);
+                  }).catch (function (err) {
+                      console.log(err);
+                  });
+          }
+        )
     .put( function (req, res)
           {
               // call a function to break up req and update price in
